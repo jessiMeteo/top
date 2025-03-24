91dae584a371642385eda574a50366df690daa33
jessimeteo <jessica.laengle@uibk.ac.at>
2025-03-24 15:16:56 +0100

    Kartenkoordinaten korregiert

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*  L. steht für Leaflet, View([long, lat], zoomfaktor) */
-var map = L.map('map').setView([-42.411667, 173.682222], 13);
+var map = L.map('map').setView([52.088889, 5.115556], 13);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        var marker = L.marker([-42.411667, 173.682222]).addTo(map);
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        var marker = L.marker([52.088889, 5.115556]).addTo(map);
+        marker.bindPopup("<b>Hello Utrecht!</b><br>I am a popup.").openPopup();
